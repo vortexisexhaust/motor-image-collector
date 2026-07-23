3b104b0eaf1599e0bb6fd304f631f825b921a262
vortexisexhaust <marcus@vortexisexhaust.com>
2026-07-23 14:12:20 +0800

    Add CFMOTO model configuration

---
+++ config.py
@@ -0,0 +1,79 @@
+"""
+Motor Image Collector
+配置文件
+"""
+
+APP_NAME = "Motor Image Collector"
+
+
+# 当前开发品牌
+BRAND = "CFMOTO"
+
+
+# CFMOTO车型数据库
+MODELS = {
+
+    "SR": [
+        "150SR",
+        "250SR",
+        "250SR FUN",
+        "250SR-R",
+        "300SR",
+        "450SR",
+        "450SR S",
+        "675SR",
+        "675SR-R",
+        "500SR VOOM",
+    ],
+
+
+    "MT": [
+        "450MT",
+        "650MT",
+        "700MT",
+        "800MT",
+        "800MT-X",
+    ],
+
+
+    "NK": [
+        "150NK",
+        "250NK",
+        "300NK",
+        "450NK",
+        "675NK",
+        "800NK",
+    ],
+
+
+    "CL-X": [
+        "250CL-X",
+        "450CL-C",
+        "700CL-X",
+    ],
+
+
+    "TR": [
+        "650TR-G",
+        "1250TR-G",
+    ],
+
+
+    "XO": [
+        "XO PAPIO Racer",
+        "XO PAPIO Trail",
+        "PAPIO ST",
+    ],
+
+
+    "ZEEHO": [
+        "AE8",
+        "AE8+",
+        "AE8 S+",
+        "AE6",
+        "AE6+",
+        "CITY SPORT",
+        "Magnet Concept",
+    ],
+
+}
